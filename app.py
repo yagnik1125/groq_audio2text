3bef469ae91ebcd58f3681776dae87845f152519
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-09-16 11:37:34 +0530

    lt

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from libretranslatepy import LibreTranslateAPI
 
 # Initialize the Groq client
 client = Groq(api_key="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")
+lt = LibreTranslateAPI("https://libretranslate.com/")
 
 # # Initialize Google Translate API
 # translator = googletrans.Translator()
