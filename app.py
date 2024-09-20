d47dd74e7fd93a61066712c054b356b45d646ca4
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-09-20 22:38:03 +0530

    llama-3.1-70b-versatile

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def translate_text(text, targ_lang):
                     "content": f"Translate this text to '{targ_lang}'. Text:{text}. ONLY RETURN TRANSLATED TEXT DO NOT WRITE ANYTHING ELSE",
                 }
             ],
-            model="llama3-70b-8192",
+            model="llama-3.1-70b-versatile",
         )
         return chat_completion.choices[0].message.content 
         # print(chat_completion.choices[0].message.content)
